c5d89fbcd2a846a9f05c65480c21b43353dd3907
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-11-21 21:47:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { token } = require('./config.json');
 
 console.log('Updating repos...')
 
-fs.readdirSync('./repo_updaters).forEach(file => {
+fs.readdirSync('./repo_updaters').forEach(file => {
   shell.exec(`python3 ./repo_updaters/${file}`)
 
 });
