49c83d9d62550a2b488fc6d4488f56c459ab4783
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-11-21 17:32:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+// Modules
+const Discord = require('discord.js');
+// create a new Discord client
+const client = new Discord.Client();
+
+const {
+	token
+} = require('./config.json');
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}\nIn ${client.guilds.cache.size} servers`);
+});
+
+
+client.on('message', async message => {
+        const prefix = "[[";
+        const end = "]]";
+	const args = message.content.slice(prefix.length).trim().split(/ +/).slice(end.length);
+	const command = args.shift().toLowerCase();
+
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (command === 'test') {
+            message.channel.send("uwu");
+        }
+});
+
+client.login(token);
