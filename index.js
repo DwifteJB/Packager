aa6e34c1f258c63806c55578b0324fba1d01d816
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-11-21 20:56:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-
 // Modules
 const Discord = require('discord.js');
-// create a new Discord client
+
 const client = new Discord.Client();
 
-const {
-	token
-} = require('./config.json');
+const { token } = require('./config.json');
 
 client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}\nIn ${client.guilds.cache.size} servers`);
 });
 
+bot.on("guildCreate", guild => {
+    let channelID;
+    let channels = guild.channels.cache;
+
+    channelLoop:
+    for (let key in channels) {
+        let c = channels[key];
+        if (c[1].type === "text") {
+            channelID = c[0];
+            break channelLoop;
+        }
+    }
+
+    let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
+
+    const lmao = new Discord.MessageEmbed()
+	    .setColor('#17bcb8')
+	    .setTitle('Packager is here!')
+	    .setDescription('Thanks for inviting packager! The bot may not respond instantly as it might be updating the repos stored locally!\nYou can find packages by typing! [[PACKAGENAME]]!')
+   	    .setTimestamp();
+    channel.send(lmao);
+});
 
 client.on('message', async message => {
         const prefix = "[[";
