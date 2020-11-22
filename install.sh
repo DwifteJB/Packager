ba904c4989faa464e7c3b789eeafecf90359a79a
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-11-22 23:01:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ rm -r ./*
 git clone -b test https://github.com/DwifteJB/PackageFinderJS update
 mv update/* ./
 rm -r update
-npm i discord.js shelljs
+npm i discord.js shelljs hastebin-gen
 chmod +x update.sh
 chmod +x install.sh
 node .
