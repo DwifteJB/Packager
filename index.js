7249f1044feddd8fbf5bf7a17edad43f66beed6e
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-11-22 13:53:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ client.on("guildCreate", guild => {
 });
 
 client.on('message', async message => {
-          const matches = message.match(/\[\[(\w*)\]\]/);
-          if (!matches) return;
-          const package matches[1];
-          for (index in BigBoss.app) {
-          if(package.trim() === BigBoss.app[index].Name.trim()) {
+        const matches = message.match(/\[\[(\w*)\]\]/);
+        if (!matches) return;
+        const package matches[1];
+        for (index in BigBoss.app) {
+        if(package.trim() === BigBoss.app[index].Name.trim()) {
 
              const lmao = new Discord.MessageEmbed()
 	             .setColor('#17bcb8')
