cec00d7231952b0aebbfd137720b71d240a3ac8c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-22 17:50:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ client.on("message", async message => {
     }
     if (!/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(args[1])) return message.channel.send("Please provide a valid URL.");
     exec(
-      `echo \'import random
+      `echo 'import random
 import os
 import csv
 import json
@@ -94,13 +94,13 @@ except:
     pass
 try:
     headers={
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        \'User-Agent\': \'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36\'
     }
     r = requests.get(f"{url}/Packages.bz2", headers=headers)
 except Exception as e:
     print("Is this a repo?")
     sys.exit(1)
-with open(f'{os.getcwd()}/data/Packages.bz2', 'wb') as f:
+with open(f\'{os.getcwd()}/data/Packages.bz2\', \'wb\') as f:
     f.write(r.content)
 try:
     zipfile = bz2.BZ2File("./data/Packages.bz2")
@@ -117,49 +117,49 @@ except:
             time.sleep(1)
 
 filepath = f"{os.getcwd()}//data/repo.csv"
-open(filepath, 'wb').write(data)
-with open(filepath, 'r+', errors='ignore') as lol:
+open(filepath, \'wb\').write(data)
+with open(filepath, \'r+\', errors=\'ignore\') as lol:
     try:
         text = lol.read()
     except Exception as e:
         print(f"Error {e} occurred")
-    if re.search('/\ ', url):
-        text = re.sub('Filename: ./debs', f'Filename: {url}debs', text)
-        text = re.sub('Filename: ./deb', f'Filename: {url}deb', text)
-        text = re.sub('Filename: deb', f'Filename: {url}deb', text)
-        text = re.sub('Filename: debs', f'Filename: {url}debs', text)
-        text = re.sub('Filename: api', f'Filename: {url}api', text)
-        text = re.sub('Filename: pool', f'Filename: {url}pool', text)
-        text = re.sub('Filename: files', f'Filename: {url}files', text)
+    if re.search(\'/\ \', url):
+        text = re.sub(\'Filename: ./debs\', f\'Filename: {url}debs\', text)
+        text = re.sub(\'Filename: ./deb\', f\'Filename: {url}deb\', text)
+        text = re.sub(\'Filename: deb\', f\'Filename: {url}deb\', text)
+        text = re.sub(\'Filename: debs\', f\'Filename: {url}debs\', text)
+        text = re.sub(\'Filename: api\', f\'Filename: {url}api\', text)
+        text = re.sub(\'Filename: pool\', f\'Filename: {url}pool\', text)
+        text = re.sub(\'Filename: files\', f\'Filename: {url}files\', text)
     else:
-        text = re.sub('Filename: ./debs', f'Filename: {url}/debs', text)
-        text = re.sub('Filename: ./deb', f'Filename: {url}/deb', text)
-        text = re.sub('Filename: deb', f'Filename: {url}/deb', text)
-        text = re.sub('Filename: debs', f'Filename: {url}/debs', text)
-        text = re.sub('Filename: api', f'Filename: {url}/api', text)
-        text = re.sub('Filename: pool', f'Filename: {url}/pool', text)
-        text = re.sub('Filename: files', f'Filename: {url}/files', text)
+        text = re.sub(\'Filename: ./debs\', f\'Filename: {url}/debs\', text)
+        text = re.sub(\'Filename: ./deb\', f\'Filename: {url}/deb\', text)
+        text = re.sub(\'Filename: deb\', f\'Filename: {url}/deb\', text)
+        text = re.sub(\'Filename: debs\', f\'Filename: {url}/debs\', text)
+        text = re.sub(\'Filename: api\', f\'Filename: {url}/api\', text)
+        text = re.sub(\'Filename: pool\', f\'Filename: {url}/pool\', text)
+        text = re.sub(\'Filename: files\', f\'Filename: {url}/files\', text)
     lol.seek(0)
-    lol.write(text.replace('\0', ' '))
+    lol.write(text.replace(\'\0\', \' \'))
     lol.truncate()
 
 final_data = {
-'url': f'{url}',
-'icon': f'{url}/CydiaIcon.png',
-'app': [],
+\'url\': f\'{url}\',
+\'icon\': f\'{url}/CydiaIcon.png\',
+\'app\': [],
 }
 app = {}
 
-with open(f'{filepath}') as csvfile:
-    data = csv.reader(csvfile, delimiter=':')
+with open(f\'{filepath}\') as csvfile:
+    data = csv.reader(csvfile, delimiter=\':\')
     for line in data:
         if len(line) == 0:
-            final_data['app'].append(app)
+            final_data[\'app\'].append(app)
             app = {}
             continue
         try:
-            if line[1].strip() in ['http', 'https']:
-                line[1] = line[1] + ':' + line[2]
+            if line[1].strip() in [\'http\', \'https\']:
+                line[1] = line[1] + \':\' + line[2]
         except:
             pass
         try:
@@ -170,11 +170,11 @@ with open(f'{filepath}') as csvfile:
       
 json_string = json.dumps(final_data)
 
-with open('/root/PackageFinderJS/repos/${args[0]}.json', 'w') as f:
+with open(\'/root/PackageFinderJS/repos/${args[0]}.json\', \'w\') as f:
     dat = json.dumps(final_data, indent=4)
     f.write(dat)
     f.close()
-    print("Done!")\' > repo_updaters/${args[0]}`, async (error, stdout, stderr) => {
+    print("Done!")' > repo_updaters/${args[0]}.py`, async (error, stdout, stderr) => {
           stdout = stdout.replace(/deb /g, '').replace(/ \.\//g,'')
         if (error) {
         const errorMsg = await haste(error, { extension: "txt", url: "https://hasteb.in" })
