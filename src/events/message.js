05cfff9273147e688573b8e9c40f43f476fdad75
BuildTools <unconfigured@null.spigotmc.org>
2020-11-22 21:05:12 -0500

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -41,8 +41,8 @@ module.exports = async (client, message) => {
     let sent = false;
     client.jsons.forEach(repo => {
         for (index in repo.app) {
-            if (package.toLowerCase() === repo.app[index].Package.toLowerCase() ||
-                package.toLowerCase() === repo.app[index].Name.toLowerCase()) {
+            if (package === repo.app[index].Name ||
+                package === repo.app[index].Package) {
                 const lmao = new Discord.MessageEmbed()
                     .setColor("#17bcb8")
                     .setDescription(repo.app[index].Description)
