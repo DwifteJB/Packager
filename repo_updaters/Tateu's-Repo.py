3fe054336f429c24421535de2e5adb8f47f1f813
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-11-23 19:27:15 +0000

    Update Tateu's-Repo.py

--- repo_updaters/Tateu's-Repo.py
+++ repo_updaters/Tateu's-Repo.py
@@ -41,7 +41,7 @@ except:
     else:
             time.sleep(1)
 filepath = f'./data/repo.csv'
-open(filepath, 'wb', errors='ignore').write(data)
+open(filepath, 'wb').write(data)
 with open(filepath, 'r+', errors='ignore') as lol:
     try:
         text = lol.read()
