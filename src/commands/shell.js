e06ddee1c684f2db85f7a79157e7a66f55f928bb
BuildTools <unconfigured@null.spigotmc.org>
2020-11-23 12:21:29 -0500

    Update shell.js

--- src/commands/shell.js
+++ src/commands/shell.js
@@ -1,6 +1,6 @@
 const { owners } = require("../config.json");
 const haste = require("hastebin-gen");
-const { spawn } = require('child_process')
+const { spawn, exec } = require('child_process')
 
 module.exports = {
   name: "shell",
@@ -11,17 +11,10 @@ module.exports = {
       return message.channel.send(`Please use the following syntax:\n\n\`\`\`${client.prefix}shell <command>\`\`\``)
     }
     message.channel.send(`Running \`${args.join(" ")}\``);
-    const cp = spawn(`${args[0]}`, args.slice(1), {
-      detached: true
-    });
-    let output = ''
-    cp.on('error', (err) => {
-      return message.channel.send(`Failed to execute command. Error: \`\`\`${err}\`\`\``);
-    });
-    cp.stdout.on('data', (data) => {
-      output += `${data}\n`
-    })
-    cp.on('close', (code) => {
+    exec(args.join(' '), (error, output, stderr) => {
+      if (error) {
+        return message.channel.send(`Failed to execute command. Error: \`\`\`${error}\`\`\``);
+      }
       if (output.length < 1) return message.channel.send('Finished with no output.')
       if (output.length > 1994) {
         haste(output).then(haste => message.channel.send("Output was too big: " + haste))
