ac0ffcc9bf342e0e11093b4a78507590b9d8764c
ToastOnAStick <64027609+ToastOnAStik@users.noreply.github.com>
2020-11-23 15:40:05 -0500

    Create invite.js

---
+++ src/commands/invite.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "invite",
+  description: "Invite me to your server",
+  type: 'utility',
+  async execute(client, message, args) {
+    //console.log("[Siri : Invite] Command has been run.");
+    let inviteEmbed = new Discord.MessageEmbed()
+      .setTitle('Invite')
+      .setColor('#18bc9b')
+      .setDescription(`[Click here to invite me](https://discord.com/oauth2/authorize?client_id=779761768447737886&scope=bot&permissions=19456)`)
+      message.channel.send(inviteEmbed);
+  }
+}
