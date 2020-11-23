84f163f3cf0e9eaf40b73712694053a29cb2c2b4
BuildTools <unconfigured@null.spigotmc.org>
2020-11-23 11:43:54 -0500

    Update addrepo.js

--- src/commands/addrepo.js
+++ src/commands/addrepo.js
@@ -16,10 +16,11 @@ module.exports = {
         if (collected.first().content.toLowerCase() == 'cancel') return message.channel.send('Cancelled!')
         message.channel.send('What is the URL of this repo? Type `cancel` to cancel the process.');
         message.channel.awaitMessages(filter, { max: 1, time: 60000 }).then(collected2 => {
-          if (!/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(collected2.first().content))
+        if (collected.first().content.toLowerCase() == 'cancel') return message.channel.send('Cancelled!') 
+          else if (!/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/.test(collected2.first().content))
             return message.channel.send("Please provide a valid URL.");
           else {
-            shell.exec(`python3 src/includes/add_repo.py ${collected.first().content} "${collected2.first().content}"`);
+            shell.exec(`python3 src/includes/add_repo.py ${collected.first().content.replace(/'/g, "\'")} "${collected2.first().content}"`);
             message.channel.send(`Added repo: \`${collected.first().content}\``);
           }
         })
