b4aab9b9f7eaf0f1d0ec4b378ff630756bed6a4c
ToastOnAStick <64027609+ToastOnAStik@users.noreply.github.com>
2020-11-23 19:58:51 -0500

    Update invite.js

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -8,7 +8,7 @@ module.exports = {
     //console.log("[Siri : Invite] Command has been run.");
     let inviteEmbed = new Discord.MessageEmbed()
       .setTitle('Invite')
-      .setColor('#18bc9b')
+      .setColor('#17bcb8')
       .setDescription(`[Click here to invite me](https://discord.com/oauth2/authorize?client_id=779761768447737886&scope=bot&permissions=19456)`)
       message.channel.send(inviteEmbed);
   }
