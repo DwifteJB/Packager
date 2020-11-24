3b3b880c0add1d82586246344bde6fed40d44f5a
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 13:09:42 -0500

    fix

--- src/commands/about.js
+++ src/commands/about.js
@@ -8,19 +8,10 @@ module.exports = {
         const inviteEmbed = new Discord.MessageEmbed()
             .setTitle('Packager')
             .setColor('#78c9df')
-            .setThumbnail('https://cdn.discordapp.com/avatars/779761768447737886/d77f492e2138703d571323fad9b5f194.webp?size=512')
-            .setDescription(`Packager allows for quick jailbreak tweak searches within Discord!
-
-**Invite**
-
-The invite link to the bot can be found [here](https://discord.com/oauth2/authorize?client_id=779761768447737886&scope=bot&permissions=19456) to add it to your server.
-
-**Usages**
-
-\`\`\`diff
-+ [[Sileo]]
-+ [[org.coolstar.sileo]]
-+ Hey guys check out [[Sileo]]!\`\`\``)
+            .setThumbnail('https://cdn.discordapp.com/avatars/779761768447737886/d77f492e2138703d571323fad9b5f194.webp?size=2048')
+            .setDescription(`Packager allows for jailbreak tweak searches within Discord!`)
+            .addField('Invite','The invite link can be found [here](https://discord.com/oauth2/authorize?client_id=779761768447737886&scope=bot&permissions=19456) to add it to your server.')
+            .addField('Usages', `\`\`\`diff\n+ [[Sileo]]\n+ [[org.coolstar.sileo]]\n+ Hey guys check out [[Sileo]]!\`\`\``)
             .setFooter(`Made by ToastOnAStick and DwifteJB`, `https://cdn.discordapp.com/avatars/779761768447737886/d77f492e2138703d571323fad9b5f194.webp?size=256`)
         
         message.channel.send(inviteEmbed);
--- src/commands/invite.js
+++ src/commands/invite.js
@@ -8,7 +8,7 @@ module.exports = {
     let inviteEmbed = new Discord.MessageEmbed()
       .setTitle('Invite')
       .setColor('#78c9df')
-      .setThumbnail('https://cdn.discordapp.com/avatars/779761768447737886/d77f492e2138703d571323fad9b5f194.webp?size=512')
+      .setThumbnail('https://cdn.discordapp.com/avatars/779761768447737886/d77f492e2138703d571323fad9b5f194.webp?size=2048')
       .setDescription(`[Click here to invite me](https://discord.com/oauth2/authorize?client_id=779761768447737886&scope=bot&permissions=19456)`)
       message.channel.send(inviteEmbed);
   }
