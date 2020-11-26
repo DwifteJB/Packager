c315d5bfbe68a4da32c70100e34bba589373e9eb
BuildTools <unconfigured@null.spigotmc.org>
2020-11-26 07:39:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ client.prefix = '!'
 client.jsons = new Collection();
 
 console.log("Updating repos...");
+shell.exec('mkdir ./repos');
 
 fs.readdirSync("./repo_updaters").forEach(file => {
   shell.exec(`python3 "./repo_updaters/${file}"`);
