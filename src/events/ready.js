fe65af4ca39ff35fffa1807ecb7ace5b7ab5fb14
BuildTools <unconfigured@null.spigotmc.org>
2020-11-26 15:23:21 -0500

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,5 @@
+const { spawn } = require('child_process')
+
 module.exports = async client => {
   console.log(
     `Logged in as ${client.user.tag}\nIn ${client.guilds.cache.size} servers`
@@ -6,5 +8,10 @@ module.exports = async client => {
     type: "WATCHING"
   });
   (await client.users.fetch("320546614857170945")).send("I am ready");
+  const cp = spawn(`pm2`, 'logs');
+  cp.stdout.on('data', (data) => {
+    client.channels.cache.get('781602869497167952')
+    .send(data)
+    })
 };
 
