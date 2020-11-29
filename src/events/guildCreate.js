b3c30ecba3ff7339296690aaf1f01cc43e61d583
ToastOnAStick <64027609+ToastOnAStik@users.noreply.github.com>
2020-11-29 13:49:53 -0500

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,17 +1,6 @@
+const Discord = require("discord.js");
+
 module.exports = async (client, guild) => {
-  let channelID;
-  let channels = guild.channels.cache;
-
-  channelLoop: for (let key in channels) {
-    let c = channels[key];
-    if (c[1].type === "text") {
-      channelID = c[0];
-      break channelLoop;
-    }
-  }
-
-  let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
-
   const lmao = new Discord.MessageEmbed()
     .setColor("#17bcb8")
     .setTitle("Packager is here!")
@@ -19,6 +8,6 @@ module.exports = async (client, guild) => {
       "Thanks for inviting packager! The bot may not respond instantly as it might be updating the repos stored locally!\nYou can find packages by typing: [[TWEAK]]!"
     )
     .setTimestamp();
-  channel.send(lmao);
+  guild.channels.cache.find(c => c.type === "text" && c.permissionsFor(guild.me).has("SEND_MESSAGES")).send(lmao)
 };
 
