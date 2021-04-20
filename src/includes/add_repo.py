8fa115a259cf94eec92000d61c90096aa1128aa2
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-04-20 17:27:30 +0200

    Feat(https://github.com/CrafterPika/ipa-library/commit/88fc8e315d8daa0c761abaad79fb8f2511006ff3): Fix big repos support

--- src/includes/add_repo.py
+++ src/includes/add_repo.py
@@ -28,7 +28,14 @@ try:
         "X-Unique-ID": "9a756301-fcbf-43f2-af7c-1eb0dc2c0c2a",
         "Connection": "keep-alive"
     }
-    r = requests.get(f"{url}/Packages.bz2", headers=headers)
+    if url == "http://apt.thebigboss.org/repofiles/cydia/" or url == "http://apt.thebigboss.org/repofiles/cydia":
+        r = requests.get(f"http://apt.thebigboss.org/repofiles/cydia/dists/stable/main/binary-iphoneos-arm/Packages.bz2", headers=headers)
+    elif url == "http://apt.modmyi.com/" or url == "http://apt.modmyi.com":
+        r = requests.get(f"http://apt.modmyi.com/dists/stable/main/binary-iphoneos-arm/Packages.bz2", headers=headers)
+    elif url == "http://cydia.zodttd.com/repo/cydia/" or url == "http://cydia.zodttd.com/repo/cydia":
+        r = requests.get(f"http://cydia.zodttd.com/repo/cydia/dists/stable/main/binary-iphoneos-arm/Packages.bz2", headers=headers)
+    else:
+        r = requests.get(f"{url}/Packages.bz2", headers=headers)
 except Exception as e:
     print("Is this a repo?")
     sys.exit(1)
@@ -55,9 +62,16 @@ with open(filepath, 'r+', errors='ignore') as lol:
         text = lol.read()
     except Exception as e:
         print(f"Error {e} occurred")
-    if re.search('/\ ', url):
+    if url == "http://apt.thebigboss.org/repofiles/cydia/dists/stable/main/binary-iphoneos-arm/" or url == "http://apt.thebigboss.org/repofiles/cydia/dists/stable/main/binary-iphoneos-arm/" or url == "http://apt.thebigboss.org/repofiles/cydia/" or url == "http://apt.thebigboss.org/repofiles/cydia":
+        text = re.sub('Filename: debs2.0/', f'Filename: http://apt.thebigboss.org/repofiles/cydia/debs2.0/', text)
+    elif url == "http://apt.modmyi.com/dists/stable/main/binary-iphoneos-arm/" or url == "http://apt.modmyi.com/dists/stable/main/binary-iphoneos-arm" or url == "http://apt.modmyi.com/" or url == "http://apt.modmyi.com":
+        text = re.sub('Filename: pool/main/', f'Filename: http://apt.modmyi.com/pool/main/', text)
+    elif url == "http://cydia.zodttd.com/repo/cydia/dists/stable/main/binary-iphoneos-arm/" or url == "http://cydia.zodttd.com/repo/cydia/dists/stable/main/binary-iphoneos-arm" or url == "http://cydia.zodttd.com/repo/cydia/" or url == "http://cydia.zodttd.com/repo/cydia":
+        text = re.sub('Filename: pool/main/', f'Filename: http://cydia.zodttd.com/repo/cydia/pool/main/', text)
+    elif re.search('/\ ', url):
         text = re.sub('Filename: ./debs', f'Filename: {url}debs', text)
         text = re.sub('Filename: ./deb', f'Filename: {url}deb', text)
+        text = re.sub('Filename: ./pkg', f'Filename: {url}pkg', text)
         text = re.sub('Filename: deb', f'Filename: {url}deb', text)
         text = re.sub('Filename: debs', f'Filename: {url}debs', text)
         text = re.sub('Filename: api', f'Filename: {url}api', text)
@@ -65,9 +79,11 @@ with open(filepath, 'r+', errors='ignore') as lol:
         text = re.sub('Filename: files', f'Filename: {url}files', text)
         text = re.sub('Filename: apt', f'Filename: {url}apt', text)
         text = re.sub('Filename: download', f'Filename: {url}download', text)
+        text = re.sub('Filename: pkg', f'Filename: {url}pkg', text)
     else:
         text = re.sub('Filename: ./debs', f'Filename: {url}/debs', text)
         text = re.sub('Filename: ./deb', f'Filename: {url}/deb', text)
+        text = re.sub('Filename: ./pkg', f'Filename: {url}/pkg', text)
         text = re.sub('Filename: deb', f'Filename: {url}/deb', text)
         text = re.sub('Filename: debs', f'Filename: {url}/debs', text)
         text = re.sub('Filename: api', f'Filename: {url}/api', text)
@@ -75,6 +91,7 @@ with open(filepath, 'r+', errors='ignore') as lol:
         text = re.sub('Filename: files', f'Filename: {url}/files', text)
         text = re.sub('Filename: apt', f'Filename: {url}/apt', text)
         text = re.sub('Filename: download', f'Filename: {url}/download', text)
+        text = re.sub('Filename: pkg', f'Filename: {url}/pkg', text)
     lol.seek(0)
     lol.write(text.replace('\0', ' '))
     lol.truncate()
