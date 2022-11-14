518ca125c3644d0a9371c02f85311d64f2120370
DwifteJB <dwifte@outlook.com>
2022-11-14 21:15:38 +0000

    pot fix

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,4 +1,6 @@
 import { RepoUpdater } from "../../src/lib/repoUpdate.js"
+import * as fs from 'fs'
+fs.mkdirSync("./repos")
 const Test = await RepoUpdater("Havoc","https://havoc.app/")
 if (Test == true) {
     console.log("test past!")
